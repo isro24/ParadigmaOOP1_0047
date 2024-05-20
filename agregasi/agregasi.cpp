dd45bcd51c143539adaa89f308a151a73d91e143
isro24 <alamat_email_anda@example.com>
2024-05-20 15:24:35 +0700

    membuat void nama anak dan ibu

---
+++ agregasi/agregasi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#include "anak.h"
+#include "ibu.h"
+
+int main(){
+    ibu* varIbu = new ibu("Dini");
+    ibu* varIbu2 = new ibu("Novi");
+    anak* varAnak1 = new anak("Tono");
+    anak* varAnak2 = new anak("Rini");
+    anak* varAnak3 = new anak("Dewi");
+
+    
+}
+
+
+
